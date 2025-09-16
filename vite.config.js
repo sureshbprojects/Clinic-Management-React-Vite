2c9213bf9985a459baa772bdd463b9ecf1619702
Autosoft <sureshbprojects@gmail.com>
2025-09-17 02:04:03 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    // You might see a 'build' section here
+    build: {
+    // This is the setting that controls the output directory
+    outDir: 'dist', 
+  },
 })
